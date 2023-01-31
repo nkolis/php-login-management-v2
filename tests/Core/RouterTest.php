a2cb0561450274994bb79e25fbc965ff39b6092a
nkolis <nurkholis010@gmail.com>
2023-01-31 22:01:09 +0700

    update router test

--- tests/Core/RouterTest.php
+++ tests/Core/RouterTest.php
@@ -2,13 +2,27 @@
 
 namespace App\PHPLoginManagement\Core;
 
+use App\PHPLoginManagement\Controller\HomeController;
 use PHPUnit\Framework\TestCase;
 
 class RouterTest extends TestCase
 {
-  public function testAddRouter()
+  public function testRouter()
   {
+    $_SERVER['PATH_INFO'] = '/';
+    $_SERVER['REQUEST_METHOD'] = 'GET';
     Router::add(method: 'GET', path: '/', controller: HomeController::class, function: 'index', middleware: []);
-    $this->expectOutputRegex("[HELLO, WORLD]");
+    Router::run();
+    $this->expectOutputRegex("[html]");
+    $this->expectOutputRegex("[PHP Login Management]");
+  }
+
+  public function testRouterNotfound()
+  {
+    $_SERVER['PATH_INFO'] = '/nofoud';
+    $_SERVER['REQUEST_METHOD'] = 'GET';
+    Router::add(method: 'GET', path: '/', controller: HomeController::class, function: 'index', middleware: []);
+    Router::run();
+    $this->expectOutputRegex("[CONTROLLER NOT FOUND]");
   }
 }
