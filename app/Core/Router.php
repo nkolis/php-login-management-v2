8c5adbb91d078ca5febb8a0c3bfcc73e668f8c93
nkolis <nurkholis010@gmail.com>
2023-01-31 15:06:51 +0700

    update router

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -1,3 +1,48 @@
 <?php
 
 namespace App\PHPLoginManagement\Core;
+
+class Router
+{
+  private static array $routes;
+
+  public static function add(string $method, string $path, string $controller, string $function, array $middleware = []): void
+  {
+    self::$routes[] = [
+      'method' => $method,
+      'path' => $path,
+      'controller' => $controller,
+      'function' => $function,
+      'middleware' => $middleware
+    ];
+  }
+
+  public static function run(): void
+  {
+
+    $url = $_SERVER['PATH_INFO'] ?? '/';
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    foreach (self::$routes as $route) {
+      $path = preg_replace('/\{\*\}/', '([0-9A-Za-z]*)', $route['path']);
+
+      $pattern = '#^' . $path . '$#';
+      if (preg_match($pattern, $url, $variables) && $method === $route['method']) {
+
+        foreach ($route['middleware'] as $middleware) {
+          $instance = new $middleware;
+          $instance->before();
+        }
+
+        array_shift($variables);
+        $controller = new $route['controller'];
+
+        call_user_func_array([$controller, $route['function']], $variables);
+        return;
+      }
+    }
+
+    http_response_code(404);
+    echo "CONTROLER NOT FOUND";
+  }
+}
