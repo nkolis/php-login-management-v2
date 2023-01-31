7c84e2a1e4576633eb1c2cb91799d1e18d698f2f
nkolis <nurkholis010@gmail.com>
2023-01-31 22:00:06 +0700

    update View

--- app/Core/View.php
+++ app/Core/View.php
@@ -6,6 +6,8 @@ class View
 {
   public static function render(string $path, array $model)
   {
+    require __DIR__ . "/../View/header.php";
     require __DIR__ . "/../View/" . $path . '.php';
+    require __DIR__ . "/../View/footer.php";
   }
 }
