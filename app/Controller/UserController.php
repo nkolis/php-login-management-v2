4a3267de72ab7ae5cf592a012165f5622cf0f894
nkolis <nurkholis010@gmail.com>
2023-02-02 17:53:01 +0700

    Update User controller

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -4,8 +4,11 @@ namespace App\PHPLoginManagement\Controller;
 
 use App\PHPLoginManagement\Config\Database;
 use App\PHPLoginManagement\Core\View;
+use App\PHPLoginManagement\Exception\ValidateException;
+use App\PHPLoginManagement\Model\UserRegisterRequest;
 use App\PHPLoginManagement\Repository\UserRepository;
 use App\PHPLoginManagement\Service\UserService;
+use Ramsey\Uuid\Uuid;
 
 class UserController
 {
@@ -28,5 +31,21 @@ class UserController
 
   public function postRegister()
   {
+    try {
+      $request = new UserRegisterRequest;
+      $uuid = Uuid::uuid4();
+      $request->id = $uuid;
+      $request->email = $_POST['email'];
+      $request->name = $_POST['name'];
+      $request->password = $_POST['password'];
+      $this->userService->register($request);
+      View::redirect('/users/login');
+    } catch (ValidateException $e) {
+
+      View::render('User/register', [
+        'title' => 'Register new user',
+        'error' => unserialize($e->getMessage())
+      ]);
+    }
   }
 }
