73bc6a8074189ee1b5da275fcf11c2fe3a52e586
nkolis <nurkholis010@gmail.com>
2023-02-02 10:48:55 +0700

    Update index

--- public/index.php
+++ public/index.php
@@ -3,11 +3,17 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\PHPLoginManagement\Controller\HomeController;
+use App\PHPLoginManagement\Controller\UserController;
 use App\PHPLoginManagement\Core\Router;
 
 
 
-
+// Home
 Router::add(method: 'GET', path: '/', controller: HomeController::class, function: 'index', middleware: []);
+
+// User Register
+Router::add(method: 'GET', path: '/users/register', controller: UserController::class, function: 'register', middleware: []);
+Router::add(method: 'POST', path: '/users/register', controller: UserController::class, function: 'postRegister', middleware: []);
+
 
 Router::run();
