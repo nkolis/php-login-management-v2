6c7265532c13c9b37f72fd4afb12a9c64d2d357b
nkolis <nurkholis010@gmail.com>
2023-02-03 22:57:42 +0700

    Add user profile update request

---
+++ app/Model/UserProfileUpdateRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\PHPLoginManagement\Model;
+
+class UserProfileUpdateRequest
+{
+  public ?string $id = null;
+  public ?string $email = null;
+  public ?string $name = null;
+}
