204afa9e3d2b6529b309229d11eeb8720779f226
nkolis <nurkholis010@gmail.com>
2023-02-09 12:14:16 +0700

    Add entity verification user

---
+++ app/Entity/VerificationUser.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\PHPLoginManagement\Entity;
+
+class VerificationUser
+{
+  public ?string $id = null;
+  public ?string $user_id = null;
+  public ?string $code = null;
+  public ?string $created_at = null;
+  public ?string $updated_at = null;
+}
